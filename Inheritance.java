21b6c3a18853663c4ae99aeca07a9dc16634036d
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2023-06-22 16:17:54 +0530

    daily learnings

---
+++ Inheritance.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+class shape{
+    public void area(){
+        System.out.println("displaying area");
+    }
+}
+
+class triangle{
+    public void area(int l, int h){
+        System.out.println(1/2*l*h);
+    }
+}
+public class Inheritance {
+    public static void main(String arsgs[]){
+        triangle t1=new triangle();
+        
+
+
+    }
+}
