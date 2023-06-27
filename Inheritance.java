efd4ca74e0e1829ded7cca310c5417dc37365673
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2023-06-27 15:26:52 +0530

    New

--- Inheritance.java
+++ Inheritance.java
@@ -1,20 +1,32 @@
 import java.util.*;
+import Bank;
 
-
-class shape{
+class shapes{
     public void area(){
         System.out.println("displaying area");
     }
 }
 
-class triangle{
+class Triangle extends shapes{
     public void area(int l, int h){
         System.out.println(1/2*l*h);
     }
 }
+
+class EquilateralTriangle extends Triangle{
+    public void area(int l, int h){
+        System.out.println(1/2*l*h);
+    }
+}
+
+class circle extends shape{
+    public void area(int r){
+        System.out.println((3.14)*r*r);
+    }
+}
 public class Inheritance {
     public static void main(String arsgs[]){
-        triangle t1=new triangle();
+        Triangle t1=new Triangle();
         
     }
 }
