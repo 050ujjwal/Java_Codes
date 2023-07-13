a3563b208cdfd45aea58ee01f568a9f92a31fe40
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2023-07-13 12:18:45 +0530

    Latest

--- LL.java
+++ LL.java
@@ -1,3 +1,24 @@
 class LL{
-    
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void addFirst(String data){
+        Node newNode=new Node(data);
+        if(head == null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public static void main(String args[]){
+        LL list=new LL();
+
+    }
 }
