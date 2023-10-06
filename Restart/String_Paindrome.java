6677e25f1fa52071b5cb3491617cb8715f5ab45e
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2023-10-07 01:07:06 +0530

    badho

--- Restart/String_Paindrome.java
+++ Restart/String_Paindrome.java
@@ -1,11 +1,12 @@
 package Restart;
 
 public class String_Paindrome {
-    
-}
-String str="";
+    public static int main(String args[]){
+        String str="";
         for(int i=S.length()-1;i>=0;i--){
             str+=S.charAt(i);
         }
         if(str.equals(S)) return 1;
         else return 0;
+    }
+}
