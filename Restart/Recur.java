627ac0da81f8181d58a286c5989ebae409a2464e
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2023-10-26 09:49:30 +0530

    new

--- Restart/Recur.java
+++ Restart/Recur.java
@@ -1,14 +1,15 @@
 package Restart;
-import java.util.*;
-
 
 public class Recur {
-    public static int printNumb(int n){
+    static void printNumb(int n){
+        if(n == 0){
+            return;
+        }
         System.out.println(n);
-        return printNumb(n-1);
+        printNumb(n-1);
     }
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        int n=5;
+        printNumb(n);
     }
 }
