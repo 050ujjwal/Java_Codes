866207690924596e84a3ad9db012e6ae92f2ca67
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2024-01-03 14:54:56 +0530

    New project

---
+++ ConnectionsCheck/FirstJDBC.java
@@ -0,0 +1,15 @@
+package ConnectionsCheck;
+import java.sql.*;
+
+class FirstJDBC {
+    public static void main(String args[]){
+        try{
+                
+
+
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
