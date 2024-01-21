ced0ae1811a705b7ab01fd69520e58128aef65e8
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2024-01-21 19:09:30 +0530

    new Question

--- First_Unique_Character_in_a_String.java
+++ First_Unique_Character_in_a_String.java
@@ -24,6 +24,15 @@ import java.util.*;
 
 public class First_Unique_Character_in_a_String {
     public static void main(String args[]){
-        
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        HashMap<Character, Integer> hm=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+        hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0)+1);
+        }
+        for(int i=0;i<s.length();i++){
+            if(hm.get(s.charAt(i))==1) System.out.println(i);
+        }
+        System.out.println(-1);
     }
 }
