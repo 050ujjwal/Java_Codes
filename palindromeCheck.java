032397e39284c38a6d759decd9bb3449d51d9e92
Ujjwal pal <71402939+050ujjwal@users.noreply.github.com>
2024-04-18 10:51:01 +0530

    new start

---
+++ palindromeCheck.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class palindromeCheck {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        String str1=sc.next();
+        for(int i=0;i<str.length();i++){
+            for(int j=0)
+            if(str.charAt(i))
+        }
+    }
+}
